ab7029b9715ebd1f3be4500a2736b8d61dadef73
Freddie Carthy <fcarthy@gmail.com>
2020-12-04 14:39:57 -0800

    modify firebase config

--- config/fire-config.js
+++ config/fire-config.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseApiKey : process.env.fb.apikey,
-  authDomain: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseAuthDomain :  process.env.fb.authdomain,
-  projectId: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseProjectId :  process.env.fb.projectid,
-  storageBucket: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseStorageBucket :  process.env.fb.storagebucket,
-  messagingSenderId: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseMessagingSenderId :  process.env.fb.messagingsenderid,
-  appId: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseAppId :  process.env.fb.appid,
-  measurementId: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseMeasurementId :  process.env.fb.measurementid
+  apiKey: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseApiKey : process.env.apikey,
+  authDomain: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseAuthDomain :  process.env.authdomain,
+  projectId: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseProjectId :  process.env.projectid,
+  storageBucket: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseStorageBucket :  process.env.storagebucket,
+  messagingSenderId: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseMessagingSenderId :  process.env.messagingsenderid,
+  appId: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseAppId :  process.env.appid,
+  measurementId: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseMeasurementId :  process.env.measurementid
 };
 
 try {
