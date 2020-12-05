e1e7a59ab054cd493f340f47f36fcbd8739e1c18
Freddie Carthy <fcarthy@gmail.com>
2020-12-04 16:02:06 -0800

    tweak firebase config

--- config/fire-config.js
+++ config/fire-config.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseApiKey : process.env.apikey,
-  authDomain: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseAuthDomain :  process.env.authdomain,
-  projectId: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseProjectId :  process.env.projectid,
-  storageBucket: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseStorageBucket :  process.env.storagebucket,
-  messagingSenderId: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseMessagingSenderId :  process.env.messagingsenderid,
-  appId: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseAppId :  process.env.appid,
-  measurementId: process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_firebaseMeasurementId :  process.env.measurementid
+  apiKey: process.env.NEXT_PUBLIC_firebaseApiKey,
+  authDomain: process.env.NEXT_PUBLIC_firebaseAuthDomain,
+  projectId: process.env.NEXT_PUBLIC_firebaseProjectId,
+  storageBucket: process.env.NEXT_PUBLIC_firebaseStorageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_firebaseMessagingSenderId,
+  appId: process.env.NEXT_PUBLIC_firebaseAppId,
+  measurementId: process.env.NEXT_PUBLIC_firebaseMeasurementId
 };
 
 try {
