725ebb02bbb6bca0b4c17f9d90565f20a9955ff5
Freddie Carthy <fcarthy@gmail.com>
2020-12-05 13:41:43 -0800

    comment out blog post logic due to router

--- pages/post.js
+++ pages/post.js
@@ -1,24 +1,24 @@
 import * as React from 'react'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
-import fire from '../config/fire-config'
+// import { useRouter } from 'next/router'
+// import fire from '../config/fire-config'
 
-import { CreatePost } from '../components/CreatePost/'
+// import { CreatePost } from '../components/CreatePost/'
 
 const Post = () => {
-  const router = useRouter();
-  const user = fire.auth().currentUser
-
-  if (!user) {
-    router.push("/")
-  }
+//   const router = useRouter();
+//   const user = fire.auth().currentUser
+// 
+//   if (!user) {
+//     router.push("/")
+//   }
 
   return (
     <>
       <Head>
         <title>Freddie Carthy - Post</title>
       </Head>
-      <CreatePost />
+      {/* <CreatePost /> */}
     </>
   )
 }
