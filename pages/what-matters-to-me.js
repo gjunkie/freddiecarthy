735e364af1320951cbd9fe6e0d8fec0b59c0ece2
Freddie Carthy <fcarthy@gmail.com>
2020-12-09 22:53:06 -0800

    add to what matters to me page

--- pages/what-matters-to-me.js
+++ pages/what-matters-to-me.js
@@ -1,15 +1,55 @@
 import * as React from 'react';
 import Head from 'next/head'
+// import { useTheme } from '../contexts/ThemeContext'
+// import { BLMGraphic } from '../components/graphics/BLM'
 import { PageTitle } from '../components/PageTitle'
 
+import styles from './BLM.module.css'
+
 const WhatMattersToMe = () => {
+  // const theme = useTheme()
+
   return (
     <>
       <Head>
         <title>Freddie Carthy - What Matters To Me</title>
       </Head>
-      <PageTitle title="What Matters To Me"></PageTitle>
-      <main>
+      <main className={styles.blm}>
+        {/* <BLMGraphic theme={theme} width="400" height="400" /> */}
+        <PageTitle title="What Matters To Me"></PageTitle>
+
+        <p>
+          What more could I possibly say about 2020 that hasn&apos;t already been said? <strong>2020 was a shitshow</strong> in more ways than I can count. <strong>It taught all of us some very important lessons in the harshest and cruelest possible ways</strong>. I&apos;d like to talk about some of those lessons here.
+        </p>
+
+        <h4>Black Lives Matter, just as much as they always have.</h4>
+        <p>
+          Look, I know how this sounds coming from me; I&apos;m a white dude working in tech living in a white city. I&apos;m beyond privilaged. What I don&apos;t want to do is add noise to this incredibly important conversation and movement. But I <i>will</i> say that it&apos;s not enough to put a bumper sticker on your car. It&apos;s not enought to say you aren&apos;t racist. It&apos;s not enough to show up and vote. <strong>You have to be actively against racism</strong>. Donate to a cause if you can. Volunteer if you can make the time. Encourage your employer to take a public stance on social issues. Contact your representative.
+        </p>
+        <p>
+          Every little bit helps. Here are some things you can do <i>right now</i>.
+        </p>
+        <ul>
+          <li>
+            Donate to the <a href="https://blacklives.help/view/tbzQRwmyzuOBZ47UArCP" target="_blank" rel="noreferrer">Black Lives Matter movement</a>.
+          </li>
+          <li>
+            Support the <a href="https://rfkhumanrights.org/" target="_blank" rel="noreferrer">Robert F. Kennedy Human Rights</a> organization.
+          </li>
+          <li>
+            <a href="https://www.house.gov/representatives/find-your-representative" target="_blank" rel="noreferrer">Find out who your Representative is</a> and contact them.
+          </li>
+        </ul>
+
+        <h4>What you say matters, and so does what you don&apos;t say</h4>
+        <p>
+          The clearest example of this is POTUS 45. But ...
+        </p>
+
+        <h4>Nurture your relationships</h4>
+        <p>
+          I&apos;m exhausted of getting bombarded with information, misinformation, breaking news, controversies, 
+        </p>
       </main>
     </>
   )
