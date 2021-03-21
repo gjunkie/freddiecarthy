52893186ac098c7b0f7c7e80275607bb9bead4af
Freddie Carthy <fcarthy@gmail.com>
2021-03-20 21:11:56 -0700

    update listening page with initial albumns

--- pages/listening/index.tsx
+++ pages/listening/index.tsx
@@ -4,9 +4,65 @@ const Listening: React.FC = () => {
 
   return (
     <main>
-      <h3>March 2021</h3>
-      <h3>2020 Albums</h3>
-      <h3>2019 Albums</h3>
+      <h3>2021</h3>
+      <ul>
+        <li>Full Circle by Hælos</li>
+      </ul>
+
+      <h3>2020</h3>
+      <ul>
+        <li>Warrior Lord by Poliça</li>
+        <li>Warpaint</li>
+      </ul>
+
+      <h3>2019</h3>
+      <ul>
+        <li>Otero War by Caveman</li>
+      </ul>
+
+      <h3>2018</h3>
+      <ul>
+        <li>Painting of a Panic Attack by Frightened Rabbit</li>
+      </ul>
+
+      <h3>2017</h3>
+      <ul>
+        <li>Soon It Will Be Cold Enough by Emancipator</li>
+        <li>Woodstock by Portugal. The man</li>
+      </ul>
+
+      <h3>2016</h3>
+      <ul>
+        <li>Faded by RIVKA (EP)</li>
+      </ul>
+
+      <h3>2012</h3>
+      <ul>
+        <li>Making Mirrors by Gotye</li>
+      </ul>
+
+      <h3>2011</h3>
+      <ul>
+        <li>Tamer Animals by Other Lives</li>
+      </ul>
+
+      <h3>2010</h3>
+      <ul>
+        <li>High Violet by The National</li>
+        <li>For Emma, Forever Ago by Bon Iver</li>
+        <li>LP4 by Ratatat</li>
+      </ul>
+
+      <h3>2009</h3>
+      <ul>
+        <li>Fever Ray debut</li>
+      </ul>
+
+      <h3>2003</h3>
+      <ul>
+        <li>Sing the Sorrow by AFI</li>
+      </ul>
+
     </main>
   )
 }
