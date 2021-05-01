c648fd37be6a34a968de1589e4a743722b488dfe
Freddie Carthy <fcarthy@gmail.com>
2021-05-01 15:01:17 -0700

    add albums to listening page

--- pages/reading/index.tsx
+++ pages/reading/index.tsx
@@ -11,23 +11,34 @@ const Reading: React.FC = () => (
     <h2>Currently Reading</h2>
     <p>A list of books I&apos;m currently reading</p>
     <ul>
-      <li>Authority - Jeff VanderMeer</li>
+      <li>Authority: A Novel (The Southern Reach Trilogy, 2)- Jeff VanderMeer</li>
     </ul>
 
     <h2>2021</h2>
     <ul>
-      <li>Annihilation - Jeff VanderMeer (<Link href="blog/annihilation-book-review">review</Link>)</li>
+      <li>Annihilation: A Novel (The Southern Reach Trilogy, 1) - Jeff VanderMeer (<Link href="blog/annihilation-book-review">review</Link>)</li>
     </ul>
 
     <h2>2016</h2>
     <ul>
       <li>A Game of Thrones - George R. R. Martin</li>
+      <li>The Expectant Father: The Ultimate Guide for Dads-to-Be - Armin A. Brott</li>
+    </ul>
+
+    <h2>2011</h2>
+    <ul>
+      <li>The Elephan Vanishes - Haruki Murakami</li>
     </ul>
 
     <h2>2010</h2>
     <ul>
       <li>A Wild Sheep Chase - Haruki Murakami</li>
       <li>The Time Traveler&apos;s Wife - Audrey Niffenegger</li>
+    </ul>
+
+    <h2>2007</h2>
+    <ul>
+      <li>The Alchemist - Paulo Coelho</li>
     </ul>
 
     <h2>2006</h2>
