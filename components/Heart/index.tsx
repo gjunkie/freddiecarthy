4717f77481e1c5192716eb48c19d0b7cf44b8858
Freddie Carthy <fcarthy@gmail.com>
2022-09-11 21:20:16 -0700

    wrap heart elements

--- components/Heart/index.tsx
+++ components/Heart/index.tsx
@@ -60,56 +60,58 @@ export const Heart = (props: Props) => {
   `
 
   return (
-    <div onClick={setLikes} className={heartClass}>
-      <span className={styles.heart}>
-        <svg x="0px" y="0px" viewBox="0 0 230 230">
-          <linearGradient id="gradient1" x1="0.5" y1="1" x2="0.5" y2="0">
-            <stop offset="6.25%" stopOpacity={userLikes > 0 ? '1' : '0'} stopColor="#d31717"/>
-            <stop offset="12.5%" stopOpacity={userLikes > 1 ? '1' : '0'} stopColor="#d31717"/>
-            <stop offset="18.75%" stopOpacity={userLikes > 2 ? '1' : '0'} stopColor="#d31717"/>
-            <stop offset="25%" stopOpacity={userLikes > 3 ? '1' : '0'} stopColor="#d31717"/>
-            <stop offset="31.25%" stopOpacity={userLikes > 4 ? '1' : '0'} stopColor="#d31717"/>
-            <stop offset="37.5%" stopOpacity={userLikes > 5 ? '1' : '0'} stopColor="#d31717"/>
-            <stop offset="43.75%" stopOpacity={userLikes > 6 ? '1' : '0'} stopColor="#d31717"/>
-            <stop offset="50%" stopOpacity={userLikes > 7 ? '1' : '0'} stopColor="#d31717"/>
-            <stop offset="56.25%" stopOpacity={userLikes > 8 ? '1' : '0'} stopColor="#d31717"/>
-            <stop offset="62.5%" stopOpacity={userLikes > 9 ? '1' : '0'} stopColor="#d31717"/>
-            <stop offset="68.75%" stopOpacity={userLikes > 10 ? '1' : '0'} stopColor="#d31717"/>
-            <stop offset="75%" stopOpacity={userLikes > 11 ? '1' : '0'} stopColor="#d31717"/>
-            <stop offset="81.25%" stopOpacity={userLikes > 12 ? '1' : '0'} stopColor="#d31717"/>
-            <stop offset="87.5%" stopOpacity={userLikes > 13 ? '1' : '0'} stopColor="#d31717"/>
-            <stop offset="93.75%" stopOpacity={userLikes > 14 ? '1' : '0'} stopColor="#d31717"/>
-            <stop offset="100%" stopOpacity={userLikes > 15 ? '1' : '0'} stopColor="#d31717"/>
-          </linearGradient>
-          <linearGradient id="gradient2" x1="0.5" y1="1" x2="0.5" y2="0">
-            <stop offset="100%" stopOpacity={1} stopColor="#ffffff"/>
-          </linearGradient>
-          <path fill="url(#gradient2)" d="M213.588,120.982L115,213.445l-98.588-92.463C-6.537,96.466-5.26,57.99,19.248,35.047l2.227-2.083
-            c24.51-22.942,62.984-21.674,85.934,2.842L115,43.709l7.592-7.903c22.949-24.516,61.424-25.784,85.936-2.842l2.227,2.083
-            C235.26,57.99,236.537,96.466,213.588,120.982z"/>
-          <path fill="url(#gradient1)" d="M213.588,120.982L115,213.445l-98.588-92.463C-6.537,96.466-5.26,57.99,19.248,35.047l2.227-2.083
-            c24.51-22.942,62.984-21.674,85.934,2.842L115,43.709l7.592-7.903c22.949-24.516,61.424-25.784,85.936-2.842l2.227,2.083
-            C235.26,57.99,236.537,96.466,213.588,120.982z"/>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-          <g></g>
-        </svg>
-      </span>
-      <span className={styles.copy}>
-       {totalLikes} {copy}
-     </span>
+    <div className={heartClass}>
+      <div onClick={setLikes}>
+        <span className={styles.heart}>
+          <svg x="0px" y="0px" viewBox="0 0 230 230">
+            <linearGradient id="gradient1" x1="0.5" y1="1" x2="0.5" y2="0">
+              <stop offset="6.25%" stopOpacity={userLikes > 0 ? '1' : '0'} stopColor="#d31717"/>
+              <stop offset="12.5%" stopOpacity={userLikes > 1 ? '1' : '0'} stopColor="#d31717"/>
+              <stop offset="18.75%" stopOpacity={userLikes > 2 ? '1' : '0'} stopColor="#d31717"/>
+              <stop offset="25%" stopOpacity={userLikes > 3 ? '1' : '0'} stopColor="#d31717"/>
+              <stop offset="31.25%" stopOpacity={userLikes > 4 ? '1' : '0'} stopColor="#d31717"/>
+              <stop offset="37.5%" stopOpacity={userLikes > 5 ? '1' : '0'} stopColor="#d31717"/>
+              <stop offset="43.75%" stopOpacity={userLikes > 6 ? '1' : '0'} stopColor="#d31717"/>
+              <stop offset="50%" stopOpacity={userLikes > 7 ? '1' : '0'} stopColor="#d31717"/>
+              <stop offset="56.25%" stopOpacity={userLikes > 8 ? '1' : '0'} stopColor="#d31717"/>
+              <stop offset="62.5%" stopOpacity={userLikes > 9 ? '1' : '0'} stopColor="#d31717"/>
+              <stop offset="68.75%" stopOpacity={userLikes > 10 ? '1' : '0'} stopColor="#d31717"/>
+              <stop offset="75%" stopOpacity={userLikes > 11 ? '1' : '0'} stopColor="#d31717"/>
+              <stop offset="81.25%" stopOpacity={userLikes > 12 ? '1' : '0'} stopColor="#d31717"/>
+              <stop offset="87.5%" stopOpacity={userLikes > 13 ? '1' : '0'} stopColor="#d31717"/>
+              <stop offset="93.75%" stopOpacity={userLikes > 14 ? '1' : '0'} stopColor="#d31717"/>
+              <stop offset="100%" stopOpacity={userLikes > 15 ? '1' : '0'} stopColor="#d31717"/>
+            </linearGradient>
+            <linearGradient id="gradient2" x1="0.5" y1="1" x2="0.5" y2="0">
+              <stop offset="100%" stopOpacity={1} stopColor="#ffffff"/>
+            </linearGradient>
+            <path fill="url(#gradient2)" d="M213.588,120.982L115,213.445l-98.588-92.463C-6.537,96.466-5.26,57.99,19.248,35.047l2.227-2.083
+              c24.51-22.942,62.984-21.674,85.934,2.842L115,43.709l7.592-7.903c22.949-24.516,61.424-25.784,85.936-2.842l2.227,2.083
+              C235.26,57.99,236.537,96.466,213.588,120.982z"/>
+            <path fill="url(#gradient1)" d="M213.588,120.982L115,213.445l-98.588-92.463C-6.537,96.466-5.26,57.99,19.248,35.047l2.227-2.083
+              c24.51-22.942,62.984-21.674,85.934,2.842L115,43.709l7.592-7.903c22.949-24.516,61.424-25.784,85.936-2.842l2.227,2.083
+              C235.26,57.99,236.537,96.466,213.588,120.982z"/>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+          </svg>
+        </span>
+        <span className={styles.copy}>
+         {totalLikes} {copy}
+       </span>
+      </div>
     </div>
   )
 }
